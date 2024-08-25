f4f30be1145cb4e963269a07b316793a650e00a7
Jonhy Olivas <jonhy.olivaspadilla@plexus.tech>
2024-08-25 16:38:41 +0200

    Agregado de inyector de dependencias de capa de aplicación.

--- ASPWeb/Program.cs
+++ ASPWeb/Program.cs
@@ -1,7 +1,11 @@
+using Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddMvc();
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
