66598909d434894e5201e328466275be37be6640
Jonhy Olivas <jonhy.olivaspadilla@plexus.tech>
2024-08-25 17:45:32 +0200

    Agregado del proyecto de presentación y creación de la vista que lista los productos.

--- Domain/Domain/Entities/Products/Product.cs
+++ Domain/Domain/Entities/Products/Product.cs
@@ -1,14 +1,22 @@
-﻿namespace Domain.Products
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Products
 {
     public class Product
     {
         public Guid Id { get; set; }
+        [Display(Name="Nombre")]
         public string Name { get; set; }
+        [Display(Name="Descripción")]
         public string Description { get; set; }
+        [Display(Name="Presentación")]
         public string Presentation { get; set; }
         public Guid Measure { get; set; }
+        [Display(Name="Costo")]
         public decimal Cost { get; set; }
+        [Display(Name="Precio")]
         public decimal Price { get; set; }
+        [Display(Name="Cantidad")]
         public int Quantity { get; set; }
     }
 }
