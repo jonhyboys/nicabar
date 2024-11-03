3faa1c5e1245207e2100abe8cbf8e08bccc67d3c
Jonhy Olivas <jonhy.olivaspadilla@plexus.tech>
2024-11-03 16:54:14 +0100

    Agregado estilo a las primeras páginas del menú. Falta Categorías.

---
+++ Presentation/wwwroot/js/Products/Products.js
@@ -0,0 +1,10 @@
+﻿(function(){
+    $(document).ready(function () {
+        $('#products').on('click', '.details', OnClick_DivDetails);
+    });
+
+    function OnClick_DivDetails() {
+        let productId = $(this).parent().attr('data-id');
+        window.location.href = "/Product/Edit/" + productId;
+    }
+}) ();
---
+++ Presentation/wwwroot/js/Tables/Tables.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    $(document).ready(function () {
+        $('#tables').on('click', '.details', OnClick_DivDetails);
+    });
+
+    function OnClick_DivDetails() {
+        let tableId = $(this).parent().attr('data-id');
+        window.location.href = "/Table/Edit/" + tableId;
+    }
+})();
